60126d69917f65a711839cff88324ee54b673b1b
lstmxx <740719284@qq.com>
2023-07-06 21:59:39 +0800

    feat: login接口

--- github_app/lib/common/git_api.dart
+++ github_app/lib/common/git_api.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
+import 'package:github_app/models/user.dart';
 
 import 'global.dart';
 
@@ -32,15 +33,31 @@ class Git {
 
     // 在调试模式下需要抓包调试，所以我们使用代理，并禁用HTTPS证书校验
     if (!Global.isRelease) {
-      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
-          (client) {
+      dio.httpClientAdapter = IOHttpClientAdapter(createHttpClient: () {
+        final client = HttpClient();
         // client.findProxy = (uri) {
         //   return 'PROXY 192.168.50.154:8888';
         // };
         //代理工具会提供一个抓包的自签名证书，会通不过证书校验，所以我们禁用证书校验
         client.badCertificateCallback =
             (X509Certificate cert, String host, int port) => true;
-      };
+        return client;
+      });
     }
+  }
+
+  Future<User> login(String login, String pwd) async {
+    String basic = 'basic ${base64Encode(utf8.encode('$login:$pwd'))}';
+    var r = await dio.get("/user",
+        options: _options.copyWith(headers: {
+          HttpHeaders.authorizationHeader: basic
+        }, extra: {
+          "noCache": true,
+        }));
+
+    dio.options.headers[HttpHeaders.authorizationHeader] = basic;
+    Global.netCache.cache.clear();
+    Global.profile.token = basic;
+    return User.fromJson(r.data);
   }
 }
