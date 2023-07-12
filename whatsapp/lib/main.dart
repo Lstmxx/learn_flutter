789faa23942dfe2751488f3fb112209b39c4b0f0
lstmxx <740719284@qq.com>
2023-07-12 10:42:39 +0800

    feat: 引入firebase

--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp/common/theme/dark_theme.dart';
 import 'package:whatsapp/common/theme/light_theme.dart';
+import 'package:whatsapp/firebase_options.dart';
 import 'package:whatsapp/modules/auth/pages/login_page.dart';
 import 'package:whatsapp/modules/auth/pages/verification_page.dart';
 import 'package:whatsapp/modules/welcome/pages/welcome_page.dart';
 
 import 'modules/auth/pages/user_info_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
